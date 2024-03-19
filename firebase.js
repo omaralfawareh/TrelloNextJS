61addbdee19bed83edc0da648c787ded13ad60a6
Omar Alfawareh <omar.alfawareh@mbc.net>
2024-03-20 01:01:12 +0300

    feature(firebase auth): setup firebase authentication configuration

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXjYmRpzu5-Wq3V774aIuMBjzy_Hn2Axw",
+  authDomain: "trellonextjs.firebaseapp.com",
+  projectId: "trellonextjs",
+  storageBucket: "trellonextjs.appspot.com",
+  messagingSenderId: "932051742732",
+  appId: "1:932051742732:web:4ef72b1f129cfa68987e29",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
