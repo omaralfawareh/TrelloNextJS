ea6078b7b65ddbad9a306254c5b981f2f0cb37d2
Omar Alfawareh <omar.alfawareh@mbc.net>
2024-03-20 01:55:11 +0300

    feature(Add AuthContext): added AuthContext to track user as a global state

---
+++ store/auth-context.js
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect } from "react";
+import { loginWithGoogle, logout } from "@/auth-util";
+import { onAuthStateChanged } from "firebase/auth";
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState();
+  const googleLogin = loginWithGoogle;
+  const signOut = logout;
+  useEffect(() => {
+    if (auth.currentUser) {
+      setUser(currentUser);
+    }
+  }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, [user]);
+  return (
+    <AuthContext.Provider value={{ user, googleLogin, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
