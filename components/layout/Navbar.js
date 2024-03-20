fb858a1a820ffb1e25c67282449b02462361091e
Omar Alfawareh <omar.alfawareh@mbc.net>
2024-03-20 12:41:02 +0300

    feature(Add login/signOut): Added conditional rendering for login and signOut on navbar

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,8 +1,12 @@
+import { useState, useContext } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
-import { useState } from "react";
+import AuthContext from "@/store/auth-context";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const authCtx = useContext(AuthContext);
+  const router = useRouter();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -12,7 +16,7 @@ function Navbar() {
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
-          href="https://flowbite.com/"
+          href=""
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img
@@ -25,13 +29,23 @@ function Navbar() {
           </span>
         </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button
-            type="button"
-            onClick={toggleMenu}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Login
-          </button>
+          {authCtx.user ? (
+            <button
+              type="button"
+              onClick={authCtx.signOut}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              SignOut
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={() => router.push("/login")}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Login
+            </button>
+          )}
           <button
             type="button"
             onClick={toggleMenu}
@@ -66,7 +80,7 @@ function Navbar() {
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                href="#"
+                href="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
@@ -75,7 +89,7 @@ function Navbar() {
             </li>
             <li>
               <Link
-                href="#"
+                href="/about"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 About
@@ -83,7 +97,7 @@ function Navbar() {
             </li>
             <li>
               <Link
-                href="#"
+                href="/services"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Services
@@ -91,7 +105,7 @@ function Navbar() {
             </li>
             <li>
               <Link
-                href="#"
+                href="/contact"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Contact
