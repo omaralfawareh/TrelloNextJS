07b1af2f202f0a95aeb7cff8e394fc51d54111a3
Omar <alfawareho@gmail.com>
2024-03-21 15:37:54 +0300

    feature(navbar refactor): implemented a new navbar

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,121 +1,67 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import Link from "next/link";
 import AuthContext from "@/store/auth-context";
 
 function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const authCtx = useContext(AuthContext);
   const router = useRouter();
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="flex justify-center bg-gray-900 fixed w-full">
+      <div className="flex content-center justify-between w-[80%] p-1 rounded-lg">
         <Link
-          href=""
-          className="flex items-center space-x-3 rtl:space-x-reverse"
+          type="button"
+          href="/login"
+          className="align-middle button bg-blue-700"
         >
-          <Image
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-            width={50}
-            height={50}
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Treplica
-          </span>
+          <span>Treplica Logo</span>
         </Link>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          {authCtx.user ? (
-            <button
-              type="button"
-              onClick={authCtx.signOut}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <ul className="flex items-center content-end flex-row pt-1 space-x-10 block">
+          <li>
+            <Link
+              href="/"
+              className="hover:text-blue-700 font-bold"
+              aria-current="page"
             >
-              SignOut
-            </button>
-          ) : (
-            <button
-              type="button"
-              onClick={() => router.push("/login")}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              <span className="text-center">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/"
+              className=" text-center hover:text-blue-700 font-bold"
             >
-              Login
-            </button>
-          )}
-          <button
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/"
+              className="align-middle hover:text-blue-700 font-bold"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+        {authCtx.user ? (
+          <Link
             type="button"
-            onClick={toggleMenu}
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            ariaControls="navbar-sticky"
-            ariaExpanded={isMenuOpen ? "true" : "false"}
+            onClick={authCtx.signOut}
+            href="/"
+            className="align-middle button bg-blue-700"
           >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              ariaHidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
-            isMenuOpen ? "block" : "hidden"
-          }`}
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                href="/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/services"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/contact"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
+            <span>SignOut</span>
+          </Link>
+        ) : (
+          <Link
+            type="button"
+            href="/login"
+            className="align-middle button bg-blue-700"
+          >
+            <span>Login</span>
+          </Link>
+        )}
       </div>
     </nav>
   );
