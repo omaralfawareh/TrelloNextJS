8e716f375df4639a1b1164b1ecdf23eda2f454df
Omar <alfawareho@gmail.com>
2024-03-21 22:55:22 +0300

    feature(fetchingBoard): fetch board from db

--- components/boards/BoardsContainer.js
+++ components/boards/BoardsContainer.js
@@ -1,8 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { db } from "@/firebase";
+import { doc, getDoc } from "firebase/firestore";
 import AuthContext from "@/store/auth-context";
 
 function BoardsContainer() {
   const authCtx = useContext(AuthContext);
+  let board = "";
+  async function fetchBoard() {
+    try {
+      const usersRef = doc(db, "users", authCtx.user.uid);
+      data = await getDoc(usersRef);
+    } catch (e) {
+      console.log(e.message);
+      return;
+    }
+    console.log(data.data().board);
+  }
+  useEffect(() => {
+    fetchBoard();
+  }, []);
+
   return (
     <>
       <h1>
