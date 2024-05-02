4e0c090ad0a6bb69657a127767fae6bd11ee0866
Omar <alfawareho@gmail.com>
2024-05-02 23:51:29 +0300

    chore(secure api key): secured firebase credentials in env local

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAXjYmRpzu5-Wq3V774aIuMBjzy_Hn2Axw",
-  authDomain: "trellonextjs.firebaseapp.com",
-  projectId: "trellonextjs",
-  storageBucket: "trellonextjs.appspot.com",
-  messagingSenderId: "932051742732",
-  appId: "1:932051742732:web:4ef72b1f129cfa68987e29",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
