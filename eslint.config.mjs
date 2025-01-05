a7367ddc737c914d9bdda8fa2cd422ce4be953d7
Omar <alfawareho@gmail.com>
2025-01-05 22:18:21 +0300

    chore: install and configure react query and its devtools

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import prettier from "eslint-config-prettier";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     rules: {
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/layout";
 import { AuthContextProvider } from "@/store/auth-context";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthContextProvider initialUser={pageProps.user}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider initialUser={pageProps.user}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthContextProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
