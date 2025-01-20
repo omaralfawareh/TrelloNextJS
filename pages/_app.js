0a52f7a1f5cc180f65517e0ba0b3d167552539ab
Omar <alfawareho@gmail.com>
2025-01-20 23:25:51 +0300

    feature: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from "@/components/layout/layout";
 import { AuthContextProvider } from "@/store/auth-context";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }) {
       <AuthContextProvider initialUser={pageProps.user}>
         <Layout>
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
       </AuthContextProvider>
       <ReactQueryDevtools initialIsOpen={false} />
